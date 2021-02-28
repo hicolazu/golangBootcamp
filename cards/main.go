fe45ba9db90d0255f5183c47299f107710d9d9d2
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2021-02-28 20:58:59 -0300

    adjusting error validation on get some deck from a file

--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	cards := newDeckFromFile("my_cards.txt")
 
-	if (cards == deck{}) {
+	if len(cards) <= 0 {
 		cards = randomDeck()
 		cards.saveToFile("my_cards_txt")
 	}
