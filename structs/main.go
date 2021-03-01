6badc41675d8b7c44b44bbae95b04e79fcc80d75
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2021-03-01 16:54:47 -0300

    Pointer Shortcut

--- structs/main.go
+++ structs/main.go
@@ -17,9 +17,6 @@ func (p person) print() {
 	fmt.Printf("%+v", p)
 }
 
-// *person: defines that pointerToPerson have to be a pointer of the type person
-// *pointerToPerson: defines that the operation will happen with the value pointed
-// by the pointer, not the adress
 func (pointerToPerson *person) updateName(newFirstName string) {
 	(*pointerToPerson).firstName = newFirstName
 }
@@ -34,11 +31,6 @@ func main() {
 		},
 	}
 
-	// Pointer rule:
-	// Turn adress to value with *adress
-	// Turn value to adress with &value
-	jimPointer := &jim
-	jimPointer.updateName("Jimmy")
-
+	jim.updateName("Jimmy")
 	jim.print()
 }
