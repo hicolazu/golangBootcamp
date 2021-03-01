57b48cde6ccf3fa6dc78ae800e2dd784d45ca8a3
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2021-03-01 16:50:55 -0300

    Pointer Operations with Go

--- structs/main.go
+++ structs/main.go
@@ -17,8 +17,11 @@ func (p person) print() {
 	fmt.Printf("%+v", p)
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+// *person: defines that pointerToPerson have to be a pointer of the type person
+// *pointerToPerson: defines that the operation will happen with the value pointed
+// by the pointer, not the adress
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func main() {
@@ -31,7 +34,11 @@ func main() {
 		},
 	}
 
-	jim.updateName("jimmy")
+	// Pointer rule:
+	// Turn adress to value with *adress
+	// Turn value to adress with &value
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
 
 	jim.print()
 }
